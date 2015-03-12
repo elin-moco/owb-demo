b224d7ff2871aa9030a1514cb46da368ab6a18c6
Eddie Lin <elin@mozilla.com>
2015-03-12 18:12:41 +0800

    Fixed write descriptor usage, force show write buttons.

--- gaia/external-apps/bleclient/js/ble_demo.js
+++ gaia/external-apps/bleclient/js/ble_demo.js
@@ -4,7 +4,6 @@ $(function () {
 
     const TAG = "BluetoothGatt";
     var REGISTER_UUID = "";
-
     var bleControl = BleCattClientControl.getInstance();
     var bluetoothManager = bleControl.getBluetooth();
     var bleManager = bleControl.getBleGatt();
@@ -42,7 +41,7 @@ $(function () {
     };
 
     var auth_req = 0;
-    var write_type = 2;
+    var write_type = 1;
 
     var front_page = -1;
 
@@ -63,14 +62,14 @@ $(function () {
     bluetoothManager.onenabled = registerCallback;
 
     bluetoothManager.ondisabled = function () {
-        Log.d(TAG, "bluetooth disabled");
+        console.info("bluetooth disabled");
         defaultAdapter = null;
     };
 
     var req = settingManager.createLock().get('bluetooth.enabled');
     req.onsuccess = function() {
         var enabled = req.result['bluetooth.enabled'];
-        Log.d(TAG, "bluetooth enabled:" + enabled);
+        console.info("bluetooth enabled:" + enabled);
         if (enabled) {
             registerCallback();
         } else {
@@ -82,13 +81,13 @@ $(function () {
     };
 
     function registerCallback() {
-        Log.d(TAG, "registerCallback");
+        console.info("registerCallback");
         defaultAdapter = null;
         var req = bluetoothManager.getDefaultAdapter();
         req.onsuccess = function bt_getAdapterSuccess() {
             defaultAdapter = req.result;
             if (defaultAdapter != null) {
-                Log.d(TAG, "defaultAdapter:" + defaultAdapter.name);
+                console.info("defaultAdapter:" + defaultAdapter.name);
                 defaultAdapter.onregisterclient = onRegisterClient;
                 defaultAdapter.onscanresult = onScanResult;
                 defaultAdapter.onconnectble = onConnectble;
@@ -116,23 +115,26 @@ $(function () {
             }
         };
         req.onerror = function bt_getAdapterFailed() {
-            Log.d(TAG, 'Can not get bluetooth adapter!');
+            console.info('Can not get bluetooth adapter!');
         };
     }
 
     function onRegisterClient(event) {
-        Log.d(TAG, "register status:" + event.status);
-        Log.d(TAG, "register client_if:" + event.client_if);
-        Log.d(TAG, "register uuid:" + event.uuid);
+        console.info("register status:" + event.status);
+        console.info("register client_if:" + event.client_if);
+        console.info("register uuid:" + event.uuid);
         if (event.status == 0) {
             regist_uuid = event.uuid;
             client_if = event.client_if;
+            console.info('regist_uuid: ' + regist_uuid);
+            console.info('client_if: ' + client_if);
             scanDevices();
+
         }
     }
 
     function onScanResult(event) {
-        Log.d(TAG, "onScanResult:" + event.bda);
+        console.info("onScanResult:" + event.bda);
         var device = {
             name : event.adv_data,
             address : event.bda,
@@ -143,8 +145,8 @@ $(function () {
     }
 
     function onConnectble(event) {
-        Log.d(TAG, "connectble status:" + event.status);
-        Log.d(TAG, "connectble conn_id:" + event.conn_id);
+        console.info("connectble status:" + event.status);
+        console.info("connectble conn_id:" + event.conn_id);
         if (event.status == 0) {
             $('#connect_state').html('SearchService...');
             conn_id = event.conn_id;
@@ -160,7 +162,7 @@ $(function () {
     }
 
     function onDisconnectble(event) {
-        Log.d(TAG, "disconnectble status:" + event.status);
+        console.info("disconnectble status:" + event.status);
         if (event.status == 0) {
             clearInterval(rssi_timer);
             rssi_timer = undefined;
@@ -177,16 +179,16 @@ $(function () {
     }
 
     function onSearchComplete(event) {
-        Log.d(TAG, "onSearchComplete status:" + event.status);
+        console.info("onSearchComplete status:" + event.status);
         service_scaning = false;
     }
 
     function onSearchResult(event) {
         $('#connect_state').html('Connected');
-        Log.d(TAG, "onSearchResult:" + event);
-        Log.d(TAG, "srvc_id_id_uuid:" + event.srvc_id_id_uuid);
-        Log.d(TAG, "srvc_id_id_inst_id:" + event.srvc_id_id_inst_id);
-        Log.d(TAG, "srvc_id_is_primary:" + event.srvc_id_is_primary);
+        console.info("onSearchResult:" + event);
+        console.info("srvc_id_id_uuid:" + event.srvc_id_id_uuid);
+        console.info("srvc_id_id_inst_id:" + event.srvc_id_id_inst_id);
+        console.info("srvc_id_is_primary:" + event.srvc_id_is_primary);
         var srvc_id = {
             uuid: event.srvc_id_id_uuid,
             inst_id: event.srvc_id_id_inst_id,
@@ -199,11 +201,11 @@ $(function () {
     }
 
     function onGetCharacteristic(event) {
-        Log.d(TAG, "onGetCharacteristic:" + event);
-        Log.d(TAG, "state:" + event.status);
-        Log.d(TAG, "char_id_uuid:" + event.char_id_uuid);
-        Log.d(TAG, "char_id_inst_id:" + event.char_id_inst_id);
-        Log.d(TAG, "char_prop:" + event.char_prop);
+        console.info("onGetCharacteristic:" + event);
+        console.info("state:" + event.status);
+        console.info("char_id_uuid:" + event.char_id_uuid);
+        console.info("char_id_inst_id:" + event.char_id_inst_id);
+        console.info("char_prop:" + event.char_prop);
 
         var char_id = {
             uuid: event.char_id_uuid,
@@ -220,9 +222,9 @@ $(function () {
     }
 
     function onGetDescriptor(event) {
-        Log.d(TAG, "descr_status:" + event.status);
-        Log.d(TAG, "descr_id_uuid:" + event.descr_id_uuid);
-        Log.d(TAG, "descr_id_inst_id:"  + event.descr_id_inst_id);
+        console.info("descr_status:" + event.status);
+        console.info("descr_id_uuid:" + event.descr_id_uuid);
+        console.info("descr_id_inst_id:"  + event.descr_id_inst_id);
 
         if (event.status != 0) {
             return;
@@ -235,55 +237,61 @@ $(function () {
     }
 
     function onGetIncludedService(event) {
-        Log.d(TAG, "onGetIncludedService:" + event);
-        Log.d(TAG, "incl_srvc_id_id_uuid:" + event.incl_srvc_id_id_uuid);
-        Log.d(TAG, "incl_srvc_id_id_inst_id:" + event.incl_srvc_id_id_inst_id);
-        Log.d(TAG, "incl_srvc_id_is_primary:" + event.incl_srvc_id_is_primary);
+        console.info("onGetIncludedService:" + event);
+        console.info("incl_srvc_id_id_uuid:" + event.incl_srvc_id_id_uuid);
+        console.info("incl_srvc_id_id_inst_id:" + event.incl_srvc_id_id_inst_id);
+        console.info("incl_srvc_id_is_primary:" + event.incl_srvc_id_is_primary);
     }
 
     function onRegisterforNotification(event) {
-        Log.d(TAG, "onRegisterforNotification registered:" + event.registered);
+        console.info("onRegisterforNotification srvc_id_id_uuid:" + event.srvc_id_id_uuid);
+        console.info("onRegisterforNotification char_id_uuid:" + event.char_id_uuid);
+        console.info("onRegisterforNotification registered:" + event.registered);
+        console.info("onRegisterforNotification status:" + event.status);
     }
 
     function onNotify(event) {
-        Log.d(TAG, "onNotify value:" + event.value);
-        Log.d(TAG, "onNotify bda:" + event.bda);
-        Log.d(TAG, "onNotify srvc_id_id_uuid:" + event.srvc_id_id_uuid);
-        Log.d(TAG, "onNotify srvc_id_id_inst_id:" + event.srvc_id_id_inst_id);
-        Log.d(TAG, "onNotify srvc_id_is_primary:" + event.srvc_id_is_primary);
-        Log.d(TAG, "onNotify char_id_uuid:" + event.char_id_uuid);
-        Log.d(TAG, "onNotify char_id_inst_id:" + event.char_id_inst_id);
-        Log.d(TAG, "onNotify len:" + event.len);
-        Log.d(TAG, "onNotify is_notify:" + event.is_notify);
+        console.info("onNotify value:" + event.value);
+        console.info("onNotify bda:" + event.bda);
+        console.info("onNotify srvc_id_id_uuid:" + event.srvc_id_id_uuid);
+        console.info("onNotify srvc_id_id_inst_id:" + event.srvc_id_id_inst_id);
+        console.info("onNotify srvc_id_is_primary:" + event.srvc_id_is_primary);
+        console.info("onNotify char_id_uuid:" + event.char_id_uuid);
+        console.info("onNotify char_id_inst_id:" + event.char_id_inst_id);
+        console.info("onNotify len:" + event.len);
+        console.info("onNotify is_notify:" + event.is_notify);
     }
 
     function onReadCharacteristic(event) {
-        Log.d(TAG, "onReadCharacteristic status:" + event.status);
-        Log.d(TAG, "onReadCharacteristic value:" + event.value);
-        Log.d(TAG, "value_type:" + event.value_type);
+        console.info("onReadCharacteristic srvc_id_id_uuid:" + event.srvc_id_id_uuid);
+        console.info("onReadCharacteristic char_id_uuid:" + event.char_id_uuid);
+        console.info("onReadCharacteristic descr_id_inst_id:" + event.descr_id_inst_id);
+        console.info("onReadCharacteristic status:" + event.status);
+        console.info("onReadCharacteristic value:" + event.value);
+        console.info("value_type:" + event.value_type);
         var value = event.value;
         $('#char_read_data').html(value);
 
     }
 
     function onWriteCharacteristic(event) {
-        Log.d(TAG, "onWriteCharacteristic status:" + event.status);
+        console.info("onWriteCharacteristic status:" + event.status);
         bleManager.executeWrite(conn_id, 0);
     }
 
     function onReadDescriptor(event) {
-        Log.d(TAG, "onReadDescriptor:" + event.value);
+        console.info("onReadDescriptor:" + event.value);
         var value = event.value;
         $('#des_read_data').html(value);
     }
 
     function onWriteDescriptor(event) {
-        Log.d(TAG, "onWriteDescriptor status:" + event.status);
+        console.info("onWriteDescriptor status:" + event.status);
         bleManager.executeWrite(conn_id, 0);
     }
 
     function onExecutWrite(event) {
-        Log.d(TAG, "onExecutWrite status:" + event.status);
+        console.info("onExecutWrite status:" + event.status);
     }
 
     function onReadRemoterssi(event) {
@@ -291,8 +299,8 @@ $(function () {
     }
 
     function onBleListen(event) {
-        Log.d(TAG, "onBleListen:" + event.status);
-        Log.d(TAG, "onBleListen:" + event.server_if);
+        console.info("onBleListen status:" + event.status);
+        console.info("onBleListen server_if:" + event.server_if);
         server_if = event.server_if;
     }
 
@@ -323,14 +331,14 @@ $(function () {
     });
 
     $('#back').on('click', function () {
-        Log.d(TAG, "click back:" + front_page);
+        console.info("click back:" + front_page);
         if (front_page < 0) {
             return;
         }
 
         switch (front_page) {
             case 0:
-                Log.d(TAG, "disconnect conn_id:" + conn_id);
+                console.info("disconnect conn_id:" + conn_id);
                 if (conn_id) {
                     bleManager.disconnectBle(client_if, bd_addr, conn_id);
                 }
@@ -386,7 +394,7 @@ $(function () {
     });
 
     $('#search_service').on('click', function() {
-        Log.d(TAG, "click service search");
+        console.info("click service search");
         if (!defaultAdapter) {
             alert("Bluetooth should be opened");
             return;
@@ -410,7 +418,7 @@ $(function () {
     });
 
     $('#char_read').on('click', function() {
-        Log.d(TAG, "click read characteristic:" + select_char_id.uuid);
+        console.info("click read characteristic:" + select_char_id.uuid);
         if (select_char_id) {
             bleManager.readCharacteristic(conn_id, select_srvc_id, select_char_id, auth_req);
         } else {
@@ -419,22 +427,23 @@ $(function () {
     });
 
     $('#char_write').on('click', function() {
-        Log.d(TAG, "click write characteristic");
+        console.info("click write characteristic");
         var cancel = {
             title: 'Cancel',
             cancel: function() {
-                Log.d(TAG, "cancel");
+                console.info("cancel");
                 Dialog.hidden();
             }
         };
         var confirm = {
             title: 'OK',
             ok: function (value) {
-                Log.d(TAG, "confirm ok:" + value);
+                console.info("confirm ok:" + value);
                 if (value) {
                     var len = value.length;
-                    Log.d(TAG, value + ":" + len);
-                    bleManager.writeCharacteristic(conn_id, select_srvc_id, select_char_id, write_type, len, auth_req, value);
+                    console.info(value + ":" + len);
+                    var charWritten = bleManager.writeCharacteristic(conn_id, select_srvc_id, select_char_id, write_type, len, auth_req, value);
+                    console.info('writeCharacteristic: ', charWritten);
                 }
                 Dialog.hidden();
             }
@@ -443,7 +452,7 @@ $(function () {
     });
 
     $('#des_read').on('click', function() {
-        Log.d(TAG, "click read descriptor:" + select_descr_id.uuid);
+        console.info("click read descriptor:" + select_descr_id.uuid);
         if (select_descr_id) {
             bleManager.readDescriptor(conn_id, select_srvc_id, select_char_id, select_descr_id, auth_req);
         } else {
@@ -452,22 +461,23 @@ $(function () {
     });
 
     $('#des_write').on('click', function() {
-        Log.d(TAG, "click write descriptor:");
+        console.info("click write descriptor:");
         var cancel = {
             title: 'Cancel',
             cancel: function() {
-                Log.d(TAG, "cancel");
+                console.info("cancel");
                 Dialog.hidden();
             }
         };
         var confirm = {
             title: 'OK',
             ok: function (value) {
-                Log.d(TAG, "confirm ok:" + value);
+                console.info("confirm ok:" + value);
                 if (value) {
                     var len = value.length;
-                    Log.d(TAG, value + ":" + len);
-                    bleManager.writeCharacteristic(conn_id, select_srvc_id, select_char_id, write_type, len, auth_req, value);
+                    console.info(value + ":" + len);
+                    var descWritten = bleManager.writeDescriptor(conn_id, select_srvc_id, select_char_id, select_descr_id, write_type, len, auth_req, value);
+                    console.info('writeDescriptor', descWritten);
                 }
                 Dialog.hidden();
             }
@@ -533,7 +543,7 @@ $(function () {
     }
 
     function showDescriptorList(show) {
-        Log.d(TAG, "show descrpt:" + show);
+        console.info("show descrpt:" + show);
         if (show) {
             $('#descriptor_list_div').show();
         } else {
@@ -615,6 +625,7 @@ $(function () {
                 showDevice(true, device);
                 bleManager.connectBle(client_if, device.address, true);
                 bd_addr = device.address;
+                console.info('bd_addr: ' + bd_addr);
             });
     }
 
@@ -670,9 +681,9 @@ $(function () {
                         break;
                     default:
                         $("#char_read").show();
-                        $("#char_write").hide();
+                        $("#char_write").show();
                         $("#des_read").show();
-                        $("#des_write").hide();
+                        $("#des_write").show();
                         break;
                 }
 
@@ -688,6 +699,7 @@ $(function () {
                     inst_id: ""
                 };
                 bleManager.getDescriptor(conn_id, select_srvc_id, select_char_id, start_descr_id);
+                console.info('registerForNotification', client_if, bd_addr, select_srvc_id, select_char_id);
                 bleManager.registerForNotification(client_if, bd_addr, select_srvc_id, select_char_id);
                 bleManager.readCharacteristic(conn_id, select_srvc_id, select_char_id, auth_req);
             });
@@ -719,5 +731,4 @@ $(function () {
         start_descr_id = descr_id;
         bleManager.getDescriptor(conn_id, select_srvc_id, select_char_id, start_descr_id);
     }
-
 });
